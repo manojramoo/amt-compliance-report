62f4b1eed19192580a256a9bd2565e2b7dbdc102
manojramoo <125301973+manojramoo@users.noreply.github.com>
2025-11-10 21:00:08 -0500

    Revert "feat: group time sheet report under reports tab (#12)" (#13)

--- src/app/dashboard-tabs/dashboard-tabs.component.ts
+++ src/app/dashboard-tabs/dashboard-tabs.component.ts
@@ -2,8 +2,8 @@ import { CommonModule } from '@angular/common';
 import { Component, ViewEncapsulation, signal } from '@angular/core';
 import { TabIdentifier } from './models/tab-content.model';
 import { LeavePlansTabComponent } from './tabs/leave-plans-tab/leave-plans-tab.component';
-import { ReportsTabComponent } from './tabs/reports-tab/reports-tab.component';
 import { ReturnToOfficeTabComponent } from './tabs/return-to-office-tab/return-to-office-tab.component';
+import { TimeSheetDefaulterTabComponent } from './tabs/time-sheet-defaulter-tab/time-sheet-defaulter-tab.component';
 
 @Component({
   selector: 'app-dashboard-tabs',
@@ -12,7 +12,7 @@ import { ReturnToOfficeTabComponent } from './tabs/return-to-office-tab/return-t
     CommonModule,
     ReturnToOfficeTabComponent,
     LeavePlansTabComponent,
-    ReportsTabComponent
+    TimeSheetDefaulterTabComponent
   ],
   templateUrl: './dashboard-tabs.component.html',
   styleUrl: './dashboard-tabs.component.css',
@@ -22,7 +22,7 @@ export class DashboardTabsComponent {
   readonly tabs: { id: TabIdentifier; label: string }[] = [
     { id: 'return-to-office', label: 'Return to Office' },
     { id: 'leave-plans', label: 'Leave Plans' },
-    { id: 'reports', label: 'Reports' }
+    { id: 'time-sheet-defaulter', label: 'Time Sheet Defaulter' }
   ];
 
   protected readonly activeTab = signal<TabIdentifier>(this.tabs[0].id);
