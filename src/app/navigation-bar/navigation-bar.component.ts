a1d3532d06db69a4a4cfa952fdfb1ad969de98b4
manojramoo <125301973+manojramoo@users.noreply.github.com>
2025-11-14 13:26:19 -0500

    Use router links in navigation bar (#17)

--- src/app/navigation-bar/navigation-bar.component.ts
+++ src/app/navigation-bar/navigation-bar.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-navigation-bar',
   standalone: true,
+  imports: [RouterLink, RouterLinkActive],
   templateUrl: './navigation-bar.component.html',
   styleUrl: './navigation-bar.component.css'
 })
